e170fc911ccb0ca47c81614c0bc74465954c478f
Raphael Diene <rdoudou.diene@gmail.com>
2022-12-10 10:27:23 +0000

    small changes

--- app/Http/Controllers/DemandeController.php
+++ app/Http/Controllers/DemandeController.php
@@ -4,11 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\DemandeStoreRequest;
 use App\Http\Requests\DemandeUpdateRequest;
-<<<<<<< HEAD
-use App\Mail\MailNouvelleDemande;
-=======
 use App\Mail\Feedback;
->>>>>>> a0db9d6fd5a31e336cf23ebf81d7f00d697d1195
 use App\Models\Demande;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -18,40 +14,6 @@ use Inertia\Inertia;
 
 class DemandeController extends Controller
 {
-<<<<<<< HEAD
-    public function store_demande(Request $request)
-    {
-        $request->validate([
-            'objet' => 'required',
-            'contenu' => 'required',
-            // 'auteur_id' => 'required',
-            // 'status' => 'required'
-        ]);
-        Demande::create([
-            'objet' => $request->objet,
-            'contenu' => $request->contenu,
-            'auteur_id' => Auth::user()->id
-
-        ]);
-        $admins=User::where('role', 'admin')->latest()->get();
-        foreach($admins as $admin){
-        Mail::to($admin->email)->send(new MailNouvelleDemande($admin->name,$request->objet));
-        }
-        return redirect()->route('dashboard');
-
-    }
-
-
-
-
-    public function admin_demandes()
-    {
-        $mes_demandes = Demande::where('admin_id', Auth::user()->id)->latest()->get();
-        foreach ($mes_demandes as $demande) {
-            $demande->auteur = User::find($demande->auteur_id)->name;
-        }
-
-=======
 
     public function admin_demandes()
     {
@@ -59,35 +21,10 @@ class DemandeController extends Controller
         foreach ($mes_demandes as $demande) {
             $demande->auteur = User::findOrFail($demande->auteur_id)->name;
         }
->>>>>>> a0db9d6fd5a31e336cf23ebf81d7f00d697d1195
         return Inertia::render('Admin/MesDemandes', ['mes_demandes' => $mes_demandes]);
     }
 
 
-<<<<<<< HEAD
-
-    public function details_demandes($id)
-    {
-        $demande = Demande::findOrFail($id);
-        $demande->auteur= User::findOrFail($demande->auteur_id);
-        // $mes_demandes = Demande::where('auteur_id', Auth::user()->id)->latest()->get();
-        // $dem_id=Demande::where('id',$id)->latest()->get();
-
-
-
-        return Inertia::render('Client/DetailDemande', ['demande' => $demande]);
-    }
-
-
-
-    public function create_demande()
-    {
-        return Inertia::render(
-            'Client/FormDemande'
-        );
-    }
-
-=======
     public function admin_demande_show(int $id)
     {
         $demande = Demande::findOrFail($id);
@@ -115,5 +52,4 @@ class DemandeController extends Controller
         $type = $request->type == 'feedback' ? 'traitée' : 'rejetée';
         Mail::to($request->demande_auteur['email'])->send(new Feedback($type,$request->feedback));
     }
->>>>>>> a0db9d6fd5a31e336cf23ebf81d7f00d697d1195
 }
